bfcd90629b3da0bdf1344c24c456447dcb5addfa
Tom Costello <tcostellosf25@gmail.com>
2018-09-18 15:42:11 -0700

    Added CORS and CSS links. Team's modules are successfully rendering

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + "/../client/dist"));
+app.use("/:id", express.static(__dirname + "/../client/dist"));
 
 let port = 3000;
 app.listen(port, () => {
